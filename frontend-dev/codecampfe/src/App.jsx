f07bfeb5e7821e74e442d4a15cd3a3f5c8302b09
Tom Tarpey <tomtarpeydev@gmail.com>
2023-10-22 23:24:56 +0100

    added a login placeholder

--- frontend-dev/codecampfe/src/App.jsx
+++ frontend-dev/codecampfe/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import HomePage from './components/HomePage'
+import Login from './components/Login'
 
 function App() {
 
   return (
     <div className='app'>
       <HomePage />
+      <Login />
     </div>
   )
 }
---
+++ frontend-dev/codecampfe/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom'
+import parseJwt from '../utils/jwtUtils'
+
+const Login = (props) => {
+    //const navigate = useNavigate();
+
+    return (
+        <div className="login">
+            <h1>Login Page</h1>
+        </div>
+    )
+}
+export default Login
