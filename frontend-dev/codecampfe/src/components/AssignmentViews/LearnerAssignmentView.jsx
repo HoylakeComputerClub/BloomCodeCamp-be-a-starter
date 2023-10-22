da6df81747e929b96e9f9f4fa522b3591ae5a3dc
Tom Tarpey <tomtarpeydev@gmail.com>
2023-10-22 23:33:42 +0100

    added a learner assignment view placeholder

---
+++ frontend-dev/codecampfe/src/components/AssignmentViews/LearnerAssignmentView.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom'
+import parseJwt from '../utils/jwtUtils'
+
+const LearnerAssignmentView = (props) => {
+    //const navigate = useNavigate();
+
+    return (
+        <div className="learnerassignment">
+            <h1>Learner Assignment View</h1>
+        </div>
+    )
+}
+export default LearnerAssignmentView
